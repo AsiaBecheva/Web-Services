e8eda4c4524e071e7960aaef7594a63150900540
AsiaBecheva <asiabecheva1@gmail.com>
2016-02-26 23:56:24 +0200

    create student controller

--- ASP.NET Web API/StudentSystem/StudentSystem.Services/Controllers/StudentsController.cs
+++ ASP.NET Web API/StudentSystem/StudentSystem.Services/Controllers/StudentsController.cs
@@ -3,11 +3,17 @@
     using StudentSystem.Data;
     using System.Web.Http;
     using System.Linq;
+    using Models;
+    using StudentSystem.Models;
     using System;
     public class StudentsController : ApiController
     {
         private IStudentSystemData db;
-        
+
+        public StudentsController() : this(new StudentsSystemData())
+        {
+        }
+
         public StudentsController(IStudentSystemData db)
         {
             this.db = db;
@@ -21,9 +27,56 @@
             return this.Ok(allStudents);
         }
 
-        public IHttpActionResult GetById(int id)
+
+        [HttpGet]
+        public IHttpActionResult GetById(int? id)
+        {
+            if (id == null)
+            {
+                return this.BadRequest("The parameter for ID cannot be null");
+            }
+
+            var student = db.Students
+                .SearchFor(st => st.StudentIdentification == id)
+                .FirstOrDefault();
+
+            if (student == null)
+            {
+                return this.BadRequest("There is no student with this ID");
+            }
+
+            return this.Ok(student);
+        }
+        
+        public IHttpActionResult Post([FromBody]StudentModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
+
+            var newStudent = new Student
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Level = model.Level
+            };
+
+            db.Students.Add(newStudent);
+            db.SaveChanges();
+
+            model.Id = newStudent.StudentIdentification;
+
+            return this.Ok(newStudent);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(int? id, StudentModel model)
         {
             throw new NotImplementedException();
         }
+
+        
+        
     }
 }
